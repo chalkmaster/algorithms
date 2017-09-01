7038fe37315accd13b5e0407ce5734df0ac60716
Charles Fortes <chalkmaster@gmail.com>
2017-09-01 17:29:17 -0300

    .

--- 1000Rainhas/index.js
+++ 1000Rainhas/index.js
@@ -1,7 +1,7 @@
 class Queen {
     /**
      * 
-     * @param {{left:Number, top: Number}} field 
+     * @param {{top:Number, left: Number}} field 
      */
     constructor(field) {
         this.field = field;
@@ -19,5 +19,46 @@ let queens = [];
 function initializeFields() {
     for (let top = topBoundary; top < bottomBoundary; top++)
         for (let left = leftBoundary; left < rightBoundary; left++)
-            emptyFields.push({ top, left });
+            emptyFields.push(`${top}x${left}`);
 }
+
+function print() {
+    let output = '';
+    for (let left = leftBoundary; left < rightBoundary; left++) {
+        for (let top = topBoundary; top < bottomBoundary; top++) {
+            if (emptyFields.indexOf(`${top}x${left}`) == -1)
+                output += "x";
+            else
+                output += "0";
+        }
+        output += "\n";
+    }
+    console.log(output);
+}
+
+function clear(ctop, cleft) {
+    emptyFields.splice(emptyFields.indexOf(`${ctop}x${cleft}`));
+
+    for (let left = cleft; left < rightBoundary; left++) {
+        const top = ctop;
+        emptyFields.splice(emptyFields.indexOf(`${top}x${left}`));
+    }
+    for (let top = ctop; top < bottomBoundary; top++) {
+        const left = cleft;
+        emptyFields.splice(emptyFields.indexOf(`${top}x${left}`));
+    }
+    for (let left = cleft; left < rightBoundary; left++) {
+        const top = ctop;
+        emptyFields.splice(emptyFields.indexOf(`${top}x${left}`));
+    }
+    for (let top = ctop; top > topBoundary; top--) {
+        const left = cleft;
+        emptyFields.splice(emptyFields.indexOf(`${top}x${left}`));
+    }
+    for (let left = cleft; left < rightBoundary; left++) {
+        const top = ctop;
+        emptyFields.splice(emptyFields.indexOf(`${top}x${left}`));
+    }
+}
+initializeFields();
+print();
