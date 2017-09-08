1fb617f634111272d77522803d7f90ed6d9c6f25
Charles Fortes <chalkmaster@gmail.com>
2017-09-08 14:48:06 -0300

    refatorando

--- 1000Rainhas/index.js
+++ 1000Rainhas/index.js
@@ -52,22 +52,63 @@ function blockFiledsFor(row, col){
     }
     board[col][row] = "[Q]";
 }
-initializeBoard();
-const run = (problemSize/2);
-let col = 0;
-let row = 0;
-while(col < run){
-    while (board[col][row] != '[ ]' && row < problemSize)
-        row++;
+
+function countBlock(row, col){
+    let qtd = 0;
+    let ldCol = 0;
+    let ldRow = 0;
+    let rdCol = 0;
+    let rdRow = 0;
+
+    if (row + col + 1 > problemSize){
+        rdCol = problemSize - 1;
+        rdRow = row - (problemSize - col - 1);
+    }
+    else {
+        rdCol = col + row;
+    }
+    if (row > col){
+        ldRow = row - col;
+    } else {
+        ldCol = col - row;
+    }
     
-    if (row < problemSize)
-        blockFiledsFor(row,col);
-    col++;
-    row++;
-    row++;    
+    for (let i = 0; i < problemSize; i++) {
+        qtd += board[i][row] != "[ ]" ? 0 : 1;
+        qtd += board[col][i] != "[ ]" ? 0 : 1;
+        if(ldCol < problemSize && ldRow < problemSize)
+            qtd += board[ldCol++][ldRow++] != "[ ]" ? 0 : 1;
+        
+        if(rdCol >= 0 && rdRow < problemSize)
+             qtd += board[rdCol--][rdRow++] != "[ ]" ? 0 : 1;
+    }
+    return qtd;
+}
+
+initializeBoard();
+let queen = 0;
+while(queen < problemSize){    
+    let min = problemSize * 4;
+    let fcol = 0;
+    let frow = 0;
+    for (let row = 0; row < problemSize; row++) {
+        for (let col = 0; col < problemSize; col++) {
+            if (board[col][row] != '[ ]')
+                continue;
+            let atual = countBlock(row, col);
+            if (atual < min){
+                min = atual;
+                fcol = col;
+                frow = row;
+            }
+        }
+    }
+    blockFiledsFor(frow,fcol);
+    queen++;
     print();
 }
 console.log('end');
+
 // const run = problemSize/2;
 // let q = 0;
 // while(q <= run+1){
